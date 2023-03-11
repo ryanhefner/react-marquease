3b30e6ad9462eddc1743f8e122a2fb75f93dfb46
Ryan Hefner <hi@ryanhefner.com>
2023-03-10 22:58:57 -0500

    Update Typescript declarations

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React, { forwardRef, useEffect, useMemo, useRef, useState } from 'react'
+import React, {
+  Component,
+  forwardRef,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 
 /**
  * react-marquease
- *
- * @param {Object} props
- * @param {ReactElement} props.children
- * @param {number} [props.defaultOffset=0]
- * @param {boolean} [props.reverse=false]
- * @param {boolean} [props.pause=false]
- * @param {number} [props.speed=1]
- * @param {LegacyRef<HTMLDivElement>} ref
- * @returns {ReactElement}
  */
 const Marquee = forwardRef(
   (
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,11 @@
-export default Marquee;
-/**
- * react-marquease
- *
- * @param {Object} props
- * @param {ReactElement} props.children
- * @param {number} [props.defaultOffset=0]
- * @param {boolean} [props.reverse=false]
- * @param {boolean} [props.pause=false]
- * @param {number} [props.speed=1]
- * @param {LegacyRef<HTMLDivElement>} ref
- * @returns {ReactElement}
- */
-declare const Marquee: import("react").ForwardRefExoticComponent<import("react").RefAttributes<any>>;
+import * as React from 'react'
+interface MarqueeProps extends React.PropsWithChildren {
+  defaultOffset?: number
+  reverse?: boolean
+  pause?: boolean
+  speed?: number
+}
+
+declare const Marquee: React.ForwardRefExoticComponent<MarqueeProps>
+
+export default Marquee
