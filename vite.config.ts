a9ec4029663df27478ce8812defc5f1a9bd1c9f4
Ryan Hefner <hi@ryanhefner.com>
2024-02-27 13:00:58 -0500

    Exclude cjs file from coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ['es/**/*', 'src/**/*.test.jsx', 'tools/**/*', 'types/**/*', 'umd/**/*'],
+      exclude: ['index.cjs', 'es/**/*', 'src/**/*.test.jsx', 'tools/**/*', 'types/**/*', 'umd/**/*'],
       reporter: ['clover', 'html', 'json', 'lcov'],
     },
     include: ['./src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
